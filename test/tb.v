88752f75898fbb0e8bd6c0b218e5fa31bb818c78
Andrew Wang <andrew.bsto@gmail.com>
2025-09-12 13:00:15 -0400

    continue fix gl_test

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example cpu_core (
+  cpu_core user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
